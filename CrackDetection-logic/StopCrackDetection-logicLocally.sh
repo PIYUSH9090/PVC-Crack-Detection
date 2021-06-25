fbb43bb5c87aea3f62365fe8491ecfe4eda21e26
PIYUSH9090 <piyush.darji@empowerinnovate.com>
2021-06-25 19:15:55 +0530

    Deleting useless directories and files

--- CrackDetection-logic/StopCrackDetection-logicLocally.sh
+++
@@ -1,7 +0,0 @@
-#StopCrackDetectionLogicLocally.sh
-echo "Stop CrackDetection Logic Locally"+ `date`
-ps ax | grep crack-detection | cut -f2 -d" " - | xargs kill -9
-ps ax | grep crack-detection | cut -f1 -d" " - | xargs kill -9
-ps ax | grep crack-detection | cut -f2 -d" " - | xargs kill -9
-ps ax | grep crack-detection | cut -f1 -d" " - | xargs kill -9
-ps ax | grep crack-detection
